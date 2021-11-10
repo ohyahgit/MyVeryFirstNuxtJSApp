49f5139e0f226d0f82c46e146c9af66c4e9f0542
Amina Shellman <aminalshellman3@gmail.com>
2021-11-10 14:53:35 -0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Hello World
+        </v-card-title>
+        <v-card-text>
+          This is my very first Nuxt.js app
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
